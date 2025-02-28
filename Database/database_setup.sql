0d0f2472e0258626204693de3af40c9d54f1909f
¨Nithin¨ <nithinnagasaigajula72@gmail.com>
2025-02-28 16:42:34 +0100

    Created predictions table with health metrics and diabetes prediction fields

--- Database/database_setup.sql
+++ Database/database_setup.sql
@@ -25,3 +25,18 @@ SELECT * FROM public.predictions LIMIT 10;
 # 7. Exit PostgreSQL Command Line
 # Exits the current PostgreSQL session
 \q
+
+CREATE TABLE predictions (
+    id SERIAL PRIMARY KEY,
+    gender VARCHAR(50),
+    age INT,
+    heart_disease INT,
+    smoking_history VARCHAR(50),
+    hbA1c_level FLOAT,
+    hypertension INT,
+    blood_glucose_level INT,
+    bmi FLOAT,
+    diabetes_prediction VARCHAR(10),
+    source VARCHAR(50),
+    prediction_date DATE
+);
